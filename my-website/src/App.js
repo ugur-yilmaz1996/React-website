eee0174c735293dbd00597037775b6e1dbdb86ee
GLOBAL\I553910 <ugur.yilmaz@sap.com>
2021-11-10 15:16:47 +0100

    adds some styling and components for future

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,9 +1,23 @@
 import './App.scss';
+import { Router, Route, Routes, Navigate } from 'react-router'
+import Dashboard from './components/dashboard/Dashboard.js';
+import About from './components/about/About';
+
 
 function App() {
   return (
+    // <Routes>
+    //   <Route path="/" exact element={<Navigate to="/dashboard" />} />
+    //   <Route path="/dashboard" element={<Dashboard />} />
+    //   <Route path="/about" element={<About />} />
+    // </Routes>
     <div>
-      <button>Style</button>
+    <header>
+      My Website
+    </header>
+    <Dashboard />
+    <input placeholder="Search ..." />
+    <button>Style</button>
     </div>
   );
 }
---
+++ my-website/src/components/dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Dashboard.scss';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <div className="container">1</div>
+            <div className="container">2</div>
+            <div className="container">3</div>
+            <div className="container">4</div>
+        </div>
+    )
+}
+
+export default Dashboard
